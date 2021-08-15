8097dd1f326160eceb5d7fadf8dc23a219bd2b71
chan032 <hochan333@naver.com>
2021-08-15 19:00:17 +0900

    210815

--- 1912.py
+++ 1912.py
@@ -1,17 +1,25 @@
-import sys
-input = sys.stdin.readline
-
-import sys
-input = sys.stdin.readline
-
-total = int(input())  # [1, 10 0000]
+total = int(input())
 numList = list(map(int, input().split()))
 
 
-dpTable = [[0] * (total + 1) for i in range(2)]
-dpTable[1][1] = numList[0]
-for i in range(2, total + 1):
-    dpTable[1][i] = max(numList[i - 1], numList[i - 1] + dpTable[1][i - 1])
-    dpTable[0][i] = max(dpTable[1][1:i]) 
+if total <= 2:
+    if total == 1:
+        print(numList[0])
+    else:
+        print(max(numList[0], numList[1], sum(numList)))
 
-print(max(dpTable[0][total], dpTable[1][total]))
+else:
+    dpTable = [[-1001] * total for _ in range(2)]
+    dpTable[1][0] = numList[0]
+    dpTable[1][1] = max(numList[1], numList[0] + numList[1])
+    dpTable[0][1] = numList[0]
+
+
+    for i in range(2, total):
+        dpTable[1][i] = max(dpTable[1][i - 1], 0) + numList[i]
+        dpTable[0][i] = max(dpTable[0][i - 1], dpTable[1][i - 1])
+        # for j in range(i):
+            # dpTable[0][i] = max(dpTable[1][:i])
+
+    print(max(dpTable[0][total - 1], dpTable[1][total - 1]))
+        
