1a8574e8d4fb3e3698f2e04b59c3d8ad61990b13
chan032 <yoonhochan3@gmail.com>
2021-09-14 18:25:04 +0900

    210914

--- 1992.py
+++ 1992.py
@@ -6,28 +6,28 @@ for i in range(N):
 answer = ''
 def division(n, array):
     global answer 
-
-    if n == 1:
-        print('ye')
-        answer += str(array[0][0])
+    if n == 0:
         return
-        
+    
     else:
         total = 0
         for arr in array:
             total += sum(arr)
             # print(type(arr))
 
-        if total == n * n:
+        if total == (n * n):
             answer += '1'
+            return
         elif total == 0:
             answer += '0'
+            return
         else:
-            # answer += '('
+            answer += '('
             division(n//2, [array[i][:n//2] for i in range(n//2)])
             division(n//2, [array[i][n//2:] for i in range(n//2)])
             division(n//2, [array[(n//2) + i][:n//2] for i in range(n//2)])
-            division(n//2, [array[i][n//2:] for i in range(n//2)])
+            division(n//2, [array[(n//2) + i][n//2:] for i in range(n//2)])
+            answer += ')'
             
 division(N, nums)
 print(answer)
