11ee50aee50e80b56526bef38e66c958593bfe43
chan032 <hochan333@naver.com>
2021-09-21 19:11:14 +0900

    210921

--- 1629.py
+++ 1629.py
@@ -2,20 +2,21 @@
 모듈러 성질 : (a * b) mod c = (a mod c * b mod c) mod c
 '''
 
-def pow1(base, exponent):
-  if exponent == 1:
-    return base
-
-  else:
-    half = pow(base, exponent // 2)
-
-    if exponent % 2 == 1: 
-      return half * half * base
-    else:  
-      return half * half
-
-
 a, b, c = map(int, input().split())
 
-print(pow(a, b))
+
+def pow(base, exponent):
+    if exponent == 1:
+        return base
+    else:
+        half = exponent // 2
+        halfPow = pow(base, half)
+
+        if exponent % 2 == 0:  # even
+            return (halfPow % c) * (halfPow % c) 
+        else:  # odd
+            return (halfPow % c) * (halfPow % c) * base
+
+print(pow(a, b)%c)
+
 
